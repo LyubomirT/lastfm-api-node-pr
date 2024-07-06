dc75970bbc98301093087f1f87adce7484ce84d4
Vedant Singhania <vedant.singhania@gmail.com>
2024-07-06 09:09:58 -0700

    files

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-  }
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+
+
+function getAllArtists(username) {
+    return axios.get(`https://www.last.fm/user/${username}`)
+        .then((response) => {
+            const $ = cheerio.load(response.data);
+
+            return $(`a[href="/user/${username}/library/artists"]`).text();
+            return $('h1').text();
+        })
+        .catch((error) => {
+            console.error('Scraping failed:', error);
+            throw error;
+        });
+}
+
+module.exports = {
+    getAllArtists
+};
---
+++ test.js
@@ -0,0 +1,15 @@
+const { getAllArtists } = require('./index'); // Adjust the path as necessary
+
+// Call the function and handle the promise it returns
+function testScrape(username) {
+    return getAllArtists(username)
+        .then((data) => {
+            console.log('Scraped data:', data);
+        })
+        .catch((error) => {
+            console.error('Scraping failed:', error);
+        });
+}
+
+// Execute the test function
+testScrape('raspberri05');
